da946b9e8309fe5509a484082152875b40413745
hmmhmmhm <hmmhmmhm@naver.com>
2021-10-24 02:01:17 +0900

    feat: Update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import notify from './core/notifier'
 import cloneTemplate from './local/cloneTemplate'
 import incompatibleCheck from './local/incompatibleCheck'
 import selectTemplate from './local/selectTemplate'
 
 void (async () => {
-  await notify()
   await incompatibleCheck()
 
   const selectedTemplate = await selectTemplate()
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+module.exports = {
+  entry: './dist/src/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'export'),
+    filename: 'frontbook-cli.js',
+    publicPath: './dist/src/index.js'
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
+}
